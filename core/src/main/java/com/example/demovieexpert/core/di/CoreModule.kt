08e18ba1d0f48d38d5b445d21c43de94a244333b
Adit Hernowo <adith@Adits-MacBook-Air.local>
2023-07-19 11:11:45 +0700

    add obfus, encrypt, certificate pinning

--- core/src/main/java/com/example/demovieexpert/core/di/CoreModule.kt
+++ core/src/main/java/com/example/demovieexpert/core/di/CoreModule.kt
@@ -4,6 +4,9 @@ import com.example.demovieexpert.core.data.source.remote.RemoteDataSource
 import com.example.demovieexpert.core.data.source.remote.network.ApiService
 import com.example.demovieexpert.core.domain.repository.IMovieRepository
 import com.example.demovieexpert.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -16,19 +19,28 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
-            MovieDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+            MovieDatabase::class.java, "Movie.db"
+        ).fallbackToDestructiveMigration().
+            openHelperFactory(factory).
+        build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/NPIMWkzcNG/MyZsVExrC6tdy5LTZzeeKg2UlnGG55UY=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
